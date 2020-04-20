8296db56c3e71a19a27ea38adb4d0af098982e15
Stephenna J Corry <Sjcstephenna@gmail.com>
2020-04-19 22:51:48 -0700

    coomit 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Grabbing all those elements!
 const fullMoon = document.getElementById('full'), 
 wanningGib = document.getElementById('wanningGib'),
 lastQuart = document.getElementById('lastQuart'),
@@ -9,23 +10,55 @@ WaxingGibbous = document.getElementById('waxingGibou'),
 imgDiv = document.getElementById('moonImg'),
 moonInfo = document.getElementById('moonInfo');
 
+// Setting phase info to hidden
 let fullMoonInfo = document.getElementById('fullMoonINFO');
 fullMoonInfo.style.visibility = 'hidden';
+
+let wanningGibINFO = document.getElementById('wanningGibINFO');
+wanningGibINFO.style.visibility = 'hidden';
+
+let lastQuartINFO = document.getElementById('lastQuartINFO');
+lastQuartINFO.style.visibility = 'hidden';
+
+let waningCrescentINFO = document.getElementById('waningCrescentINFO');
+waningCrescentINFO.style.visibility = 'hidden';
+
+let newMoonINFO = document.getElementById('newMoonINFO');
+newMoonINFO.style.visibility = 'hidden';
+
+let waxingCrescentINFO = document.getElementById('waxingCrescentINFO');
+waxingCrescentINFO.style.visibility = 'hidden';
+
+let firstQuarterINFO = document.getElementById('firstQuarterINFO');
+firstQuarterINFO.style.visibility = 'hidden';
+
+let waxingGibINFO = document.getElementById('waxingGibINFO');
+waxingGibINFO.style.visibility = 'hidden';
+
 
 
 
 
 function fullMoonE(){
-    let fullMoonImg = document.createElement('img')
+    let fullMoonImg = document.createElement('img');
     fullMoonImg.src = './img/full-moon.jpg';
     imgDiv.appendChild(fullMoonImg);
     fullMoonInfo.style.visibility = 'visible';
     
 }
 
+function waningGibE(){
+    let waningGibImg = document.createElement('img');
+    waningGibImg.src = './img/wanning-gibbous.jpg';
+    imgDiv.appendChild(waningGibImg);
+    wanningGibINFO.style.visibility = 'visible';
+}
 
 fullMoon.addEventListener('click', fullMoonE, {once : true});
 
-let fullMoonImg = new Image();
-    fullMoonImg.src = './img/full-moon.jpg';
+
+wanningGib.addEventListener('click', waningGibE, {once : true});
+
+// let fullMoonImg = new Image();
+//     fullMoonImg.src = './img/full-moon.jpg';
 //     imgDiv.appendChild(fullMoonImg);
